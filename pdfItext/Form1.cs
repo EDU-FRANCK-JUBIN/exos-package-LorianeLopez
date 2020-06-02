eba8de336b31be1c4be3bb3b395b97e5920a1848
LorianeLopez <33418065+LorianeLopez@users.noreply.github.com>
2020-06-02 12:17:02 +0200

    Exercice PDF

---
+++ pdfItext/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+namespace pdfItext
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var exportFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var exportFile = System.IO.Path.Combine(exportFolder, "Test.pdf");
+
+            using (var writer = new PdfWriter(exportFile))
+            {
+                using (var pdf = new PdfDocument(writer))
+                {
+                    var doc = new Document(pdf);
+                    doc.Add(new Paragraph(textBox1.Text));
+                    doc.Close();
+                }
+            }
+            label1.Text = "Le pdf à été généré !";
+            textBox1.Clear();
+        }
+    }
+}
